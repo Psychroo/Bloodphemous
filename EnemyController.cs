d42d6ae9ab519a5d68e08559be86a5d798a29320
wwolfjk <wwolfjk@gmail.com>
2025-07-12 17:19:19 +0200

    enemycontroller

---
+++ EnemyController.cs
@@ -0,0 +1,41 @@
+public class EnemyController : using UnityEngine;
+
+public class Untitled-2 : MonoBehaviour {
+    
+}
+{
+    private float speed = 0;
+    private float interval = 0;
+
+    private int hp = 3;
+
+    void start() { }
+
+
+    void update()
+    {
+        if (interval == 0)
+        {
+
+            flip;
+        }
+        if (hp <= 0)
+        {
+
+            this.setActive(false);
+        }
+
+        
+
+
+    }
+
+
+
+
+
+
+
+
+
+ }
