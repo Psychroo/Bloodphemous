48180b61956d0fcdde740731a4115e25f7186200
Psychroo <diegooprez@gmail.com>
2025-07-12 16:36:28 +0200

    eldeverda

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    public float move;
+
+    public bool isGrounded;
+
+    public LayerMask groundLayers;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    public float jumpForce;
+
+    public bool isFacingRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayers);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+            }
+
+        }
+    }
+    void FixedUpdate()
+    {
+        
+      
+
+            move = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(move * speed, rb.velocity.y);
+       
+
+
+
+
+
+
+
+        if ((move > 0.0f && isFacingRight == false)  (move < 0.0f && isFacingRight == true))
+
+
+
+
+
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 playerScale = transform.localScale;
+        playerScale.x = playerScale.x * -1;
+        transform.localScale = playerScale;
+    }
+
+
+}
